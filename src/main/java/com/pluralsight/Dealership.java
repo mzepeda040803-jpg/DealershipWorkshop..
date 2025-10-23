4981e6a236306120d8ba95a1e947fd8c0a9af7d6
m.zepeda040803-jpg <m.zepeda040803@gmai.com>
2025-10-23 16:23:42 -0400

    initial commit

--- src/main/java/com/pluralsight/Dealership.java
+++ src/main/java/com/pluralsight/Dealership.java
@@ -1,4 +1,24 @@
 package com.pluralsight;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Dealership {
+    private String name;
+    private String address;
+    private String phone;
+    private ArrayList<Vehicle> vehicles;
+
+
+    public Dealership(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.vehicles = new ArrayList<>();
+    }
+
+    public ArrayList<Vehicle> getVehicles() {
+        return vehicles;
+    }
 }
